fdf5780906c746f6aa9be5bd29517fa8d1269d82
manar sameh <152812570+ManarSameh@users.noreply.github.com>
2025-04-22 14:33:33 +0200

    signIn.js

--- js/signIn.js
+++ js/signIn.js
@@ -7,25 +7,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
             const username = signInForm.querySelector("input[name='username']").value.trim();
             const password = signInForm.querySelector("input[name='password']").value.trim();
+            const rememberMe = signInForm.querySelector("input[name='Remember']").checked;
 
             if (!username || !password) {
                 alert("Please fill in all fields.");
                 return;
             }
 
-            
             const users = JSON.parse(localStorage.getItem("users")) || [];
 
-           
             const matchedUser = users.find(user => user.username === username && user.password === password);
 
             if (matchedUser) {
                 alert("Login successful!");
 
                 
-                localStorage.setItem("user", JSON.stringify(matchedUser));
+                if (rememberMe) {
+                    localStorage.setItem("user", JSON.stringify(matchedUser));
+                } else {
+                    sessionStorage.setItem("user", JSON.stringify(matchedUser));
+                }
 
-                
                 if (matchedUser.role === "admin") {
                     window.location.href = "homeAdmin.html";
                 } else {
